d881f4039baa42aa39eb5fd1b21c8a9ba09ad69d
kaltinril <jeremy.swartwood@gmail.com>
2015-08-17 21:18:21 -0800

    Initial commit

---
+++ android/src/com/binaryjeremys/tweentest/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.binaryjeremys.tweentest.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.binaryjeremys.tweentest.TweenTest;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TweenTest(), config);
+	}
+}
---
+++ desktop/src/com/binaryjeremys/tweentest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.binaryjeremys.tweentest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.binaryjeremys.tweentest.TweenTest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TweenTest(), config);
+	}
+}
